52a9ce8dc19be8a80d09acc7a508bcb6827cd0e4
jqklony <jqklony@gmail.com>
2026-04-07 17:11:50 -0400

    fix: restore Agenda.jsx + add PlanGate/CIE imports to 7 pages (fix placeholder damage)

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -1,1 +1,4 @@
-PLACEHOLDER_AGENDA
+import React from 'react';
+import { PlanGate } from '../components/AppComponents.jsx';
+import { initialOccupPatientState, initialGeneralPatientState } from '../data/initialState.js';
+import { getSpanishDate } from '../utils/helpers.js';
